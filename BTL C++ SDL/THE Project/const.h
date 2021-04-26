944456a5e97f3601d97e201c9c8a605d403538b3
PainterHalver <hieptrang13042002@gmail.com>
2021-04-26 19:02:33 +0700

    ok2

--- BTL C++ SDL/THE Project/const.h
+++ BTL C++ SDL/THE Project/const.h
@@ -3,15 +3,15 @@
 const int FPS = 60;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const int ROWS = 16;
-const int COLS = 16;
+const int ROWS = 16; //16
+const int COLS = 16; //16
 const int WIDTH = 30;
 const int HEIGHT = WIDTH;
 const double GRID_START_X = (double)(SCREEN_WIDTH-COLS*WIDTH)/2;
 const double GRID_START_Y = (double)(SCREEN_HEIGHT-ROWS*HEIGHT)/2;
 const double GRID_WIDTH = COLS*WIDTH;
 const double GRID_HEIGHT = ROWS*HEIGHT;
-const int TOTALBOMBS = 27;
+const int TOTALBOMBS = 27; //27
 
 
 
--- BTL C++ SDL/THE Project/gameControl.cpp
+++ BTL C++ SDL/THE Project/gameControl.cpp
@@ -33,47 +33,7 @@ bool checkSurround(int i, int j, Cell **grid){
     return true;
 }
 
-void gameControl(SDL_Renderer* &renderer) {
-    srand(time(0));
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 155);
-    SDL_RenderClear(renderer);
-
-    //init imgs
-    SDL_Surface* no1 = SDL_LoadBMP("1.bmp");
-    SDL_Surface* no2 = SDL_LoadBMP("2.bmp");
-    SDL_Surface* no3 = SDL_LoadBMP("3.bmp");
-    SDL_Surface* no4 = SDL_LoadBMP("4.bmp");
-    SDL_Surface* no5 = SDL_LoadBMP("5.bmp");
-    SDL_Surface* no6 = SDL_LoadBMP("6.bmp");
-    SDL_Surface* no7 = SDL_LoadBMP("7.bmp");
-    SDL_Surface* no8 = SDL_LoadBMP("8.bmp");
-    SDL_Surface* no9 = SDL_LoadBMP("THE_mine.bmp");
-    SDL_Surface* no10 = SDL_LoadBMP("youwin.bmp");
-    SDL_Surface* playAgainButton = SDL_LoadBMP("PlayAgain.bmp");
-    SDL_Texture* playAgainButtonTexture = SDL_CreateTextureFromSurface(renderer, playAgainButton);
-    SDL_Texture* numberTexture[10];
-    numberTexture[0] = SDL_CreateTextureFromSurface(renderer, no1);
-    numberTexture[1] = SDL_CreateTextureFromSurface(renderer, no2);
-    numberTexture[2] = SDL_CreateTextureFromSurface(renderer, no3);
-    numberTexture[3] = SDL_CreateTextureFromSurface(renderer, no4);
-    numberTexture[4] = SDL_CreateTextureFromSurface(renderer, no5);
-    numberTexture[5] = SDL_CreateTextureFromSurface(renderer, no6);
-    numberTexture[6] = SDL_CreateTextureFromSurface(renderer, no7);
-    numberTexture[7] = SDL_CreateTextureFromSurface(renderer, no8);
-    numberTexture[8] = SDL_CreateTextureFromSurface(renderer, no9);
-    numberTexture[9] = SDL_CreateTextureFromSurface(renderer, no10);
-    SDL_FreeSurface(no1);
-    SDL_FreeSurface(no2);
-    SDL_FreeSurface(no3);
-    SDL_FreeSurface(no4);
-    SDL_FreeSurface(no5);
-    SDL_FreeSurface(no6);
-    SDL_FreeSurface(no7);
-    SDL_FreeSurface(no8);
-    SDL_FreeSurface(no9);    SDL_FreeSurface(no10);
-    SDL_FreeSurface(playAgainButton);
-
-    //khoi tao grid
+Cell** initGrid(SDL_Renderer* &renderer, SDL_Texture* mineTexture, SDL_Texture** numberTexture){
     //Cell grid[ROWS][COLS];
     Cell **grid;
     grid = new Cell *[ROWS];
@@ -81,39 +41,28 @@ void gameControl(SDL_Renderer* &renderer) {
         grid[i] = new Cell[COLS];
     }
 
-
-    srand(time(0));
+    //render start grid
     for(int i = 0; i < ROWS; ++i) {
         for(int j = 0; j < COLS; ++j) {
             Cell temp(i, j, i*WIDTH+GRID_START_X, j*HEIGHT+GRID_START_Y, WIDTH-1, HEIGHT-1);
             grid[i][j] = temp;
-            grid[i][j].show(renderer, numberTexture[8], numberTexture);
+            grid[i][j].show(renderer, mineTexture, numberTexture);
         }
     }
 
     //SPREAD THE BOMBS
     int bombCreated = 0;
-
     while(bombCreated < TOTALBOMBS) {
         int i = betweenZeroAnd(ROWS);
         int j = betweenZeroAnd(COLS);
-
-        //if(grid[i][j].hasBomb == false) {
-            //grid[i][j].hasBomb = true;
-            //grid[i][j].neighborCount = -1;
-            //bombCreated++;
-        //}
-
         if(!(grid[i][j].hasBomb) && checkSurround(i,j, grid)){
             grid[i][j].hasBomb = true;
             grid[i][j].neighborCount = -1;
             bombCreated++;
         }
-
     }
 
-
-    // COUNT NEIGHBORS
+    // COUNT NEIGHBORS and ASSIGN TO CELL
     for(int i = 0; i < ROWS; ++i) {
         for(int j = 0; j < COLS; ++j) {
             if(grid[i][j].hasBomb) continue;
@@ -132,6 +81,54 @@ void gameControl(SDL_Renderer* &renderer) {
             grid[i][j].neighborCount = total;
         }
     }
+    return grid;
+}
+
+void gameControl(SDL_Renderer* &renderer) {
+    srand(time(0));
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 155);
+    SDL_RenderClear(renderer);
+
+    //INIT IMGS
+    SDL_Surface* no1 = SDL_LoadBMP("1.bmp");
+    SDL_Surface* no2 = SDL_LoadBMP("2.bmp");
+    SDL_Surface* no3 = SDL_LoadBMP("3.bmp");
+    SDL_Surface* no4 = SDL_LoadBMP("4.bmp");
+    SDL_Surface* no5 = SDL_LoadBMP("5.bmp");
+    SDL_Surface* no6 = SDL_LoadBMP("6.bmp");
+    SDL_Surface* no7 = SDL_LoadBMP("7.bmp");
+    SDL_Surface* no8 = SDL_LoadBMP("8.bmp");
+    SDL_Surface* mine = SDL_LoadBMP("THE_mine.bmp");
+    SDL_Surface* youwin = SDL_LoadBMP("youwin.bmp");
+    SDL_Surface* playAgainButton = SDL_LoadBMP("PlayAgain.bmp");
+
+    SDL_Texture* playAgainButtonTexture = SDL_CreateTextureFromSurface(renderer, playAgainButton);
+    SDL_Texture* mineTexture = SDL_CreateTextureFromSurface(renderer, mine);
+    SDL_Texture* youwinTexture = SDL_CreateTextureFromSurface(renderer, youwin);
+
+    SDL_Texture* numberTexture[8];
+    numberTexture[0] = SDL_CreateTextureFromSurface(renderer, no1);
+    numberTexture[1] = SDL_CreateTextureFromSurface(renderer, no2);
+    numberTexture[2] = SDL_CreateTextureFromSurface(renderer, no3);
+    numberTexture[3] = SDL_CreateTextureFromSurface(renderer, no4);
+    numberTexture[4] = SDL_CreateTextureFromSurface(renderer, no5);
+    numberTexture[5] = SDL_CreateTextureFromSurface(renderer, no6);
+    numberTexture[6] = SDL_CreateTextureFromSurface(renderer, no7);
+    numberTexture[7] = SDL_CreateTextureFromSurface(renderer, no8);
+
+    SDL_FreeSurface(no1);
+    SDL_FreeSurface(no2);
+    SDL_FreeSurface(no3);
+    SDL_FreeSurface(no4);
+    SDL_FreeSurface(no5);
+    SDL_FreeSurface(no6);
+    SDL_FreeSurface(no7);
+    SDL_FreeSurface(no8);
+    SDL_FreeSurface(mine);    SDL_FreeSurface(youwin);
+    SDL_FreeSurface(playAgainButton);
+
+    //INIT GRID
+    Cell **grid = initGrid(renderer, mineTexture, numberTexture);
 
     //Always keep the "play again" button
     SDL_Rect playAgainRect = {10, 10,107,29}; // Positioning the button
@@ -141,27 +138,26 @@ void gameControl(SDL_Renderer* &renderer) {
     bool isRunning = true;
     Cell* curHover = new Cell;
     SDL_RenderPresent(renderer);
-
-
     int waittime = 1000.0f/FPS;
     int framestarttime = 0;
     int delaytime;
-
-
     int cellLeft = ROWS*COLS;
+
+    //MAIN LOOP
     while (isRunning){
         // CHECK WIN
         if(cellLeft == TOTALBOMBS){
             SDL_Rect rect = {300,10,200, 30};
-            SDL_RenderCopy(renderer, numberTexture[9], NULL, &rect);
+            SDL_RenderCopy(renderer, youwinTexture, NULL, &rect);
         }
-
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT){
                 isRunning = false;
                 break;
             }
+            //MOUSE EVENTS
             else if (e.type == SDL_MOUSEMOTION){
+                //HOVER CELL
                 double x = e.button.x;
                 double y = e.button.y;
                 curHover->unhover(renderer);
@@ -169,7 +165,6 @@ void gameControl(SDL_Renderer* &renderer) {
                     curHover->unhover(renderer);
                     curHover = &grid[(int)(x-GRID_START_X)/WIDTH][(int)(y-GRID_START_Y)/HEIGHT];
                     curHover->hover(renderer);
-
                 }
             }
             else if (e.type == SDL_MOUSEBUTTONDOWN) {
@@ -177,11 +172,11 @@ void gameControl(SDL_Renderer* &renderer) {
                 double y = e.button.y;
                 if(x > GRID_START_X && x < GRID_START_X+GRID_WIDTH && y > GRID_START_Y && y < GRID_START_Y+GRID_HEIGHT){
                     if(!(grid[(int)(x-GRID_START_X)/WIDTH][(int)(y-GRID_START_Y)/HEIGHT].hasBomb)){
-                       grid[(int)(x-GRID_START_X)/WIDTH][(int)(y-GRID_START_Y)/HEIGHT].reveal(grid, ROWS, COLS, renderer, numberTexture[8], numberTexture, cellLeft);
+                       grid[(int)(x-GRID_START_X)/WIDTH][(int)(y-GRID_START_Y)/HEIGHT].reveal(grid, ROWS, COLS, renderer, mineTexture, numberTexture, cellLeft);
                     } else {
                         for(int i = 0; i < ROWS; ++i) {
                             for(int j = 0; j < COLS; ++j) {
-                                grid[i][j].reveal(grid, ROWS, COLS, renderer, numberTexture[8], numberTexture, cellLeft);
+                                grid[i][j].reveal(grid, ROWS, COLS, renderer, mineTexture, numberTexture, cellLeft);
                             }
                         }
                     }
@@ -193,11 +188,11 @@ void gameControl(SDL_Renderer* &renderer) {
         }
         SDL_RenderPresent(renderer);
 
+        //SET FPS DELAY
         delaytime = waittime - (SDL_GetTicks() - framestarttime);
         if(delaytime > 0)
             SDL_Delay((int)delaytime);
         framestarttime = SDL_GetTicks();
     }
-
     delete [] grid;
 }
